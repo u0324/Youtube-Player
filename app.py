cfaa578e28351c244b5d552112b59317a9d37673
u0324 <tamago2605@gmail.com>
2026-02-20 00:15:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,15 @@ def index():
     if request.method == 'POST':
         v_id = get_video_id(request.form.get('url', ''))
         if v_id:
-            # タグを文字列結合で作成（制限回避）
             src_url = f"https://www.youtube.com/embed/{v_id}?playlist={v_id}&loop=1&autoplay=1"
-            player_content = f''
+            player_content = f'<div><iframe width="100%" height="400" src="{src_url}" frameborder="0" allow="autoplay"></iframe></div>'
         else:
             player_content = "無効なURLです"
 
-    # 最小限の構成を文字列で結合して返す
-    head = ""
-    title = "LoopTube Player"
-    form = ""
-    footer = ""
+    head = "<html><head><title>LoopTube</title></head><body style='text-align:center;padding:20px;'>"
+    title = "<h1>LoopTube Player</h1>"
+    form = "<form method='POST'><input type='text' name='url' style='width:70%' placeholder='URLを貼る'><button type='submit'>PLAY</button></form>"
+    footer = "</body></html>"
     
     return head + title + player_content + form + footer
 
